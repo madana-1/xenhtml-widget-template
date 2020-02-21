3528b0d7fd0d623bf15203baac258f5f4ac5eb21
madana-1 <contact.madanai@gmail.com>
2020-02-22 01:21:23 +0900

    Initialized

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
+html,
+body,
+#app {
+  margin: 0;
+  width: 100vw;
+  height: 100vw;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  user-select: none;
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicPath: './',
+  pages: {
+    index: {
+      entry: 'src/index.js',
+      template: 'public/Wallpaper.html',
+      filename: 'Wallpaper.html'
+    }
+  }
+}
